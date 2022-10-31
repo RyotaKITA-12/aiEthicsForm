226308db81ae6c09f3338375b06233daab3790ca
RyotaKITA-12 <s2022007@stu.musashino-u.ac.jp>
2022-10-31 23:53:32 +0900

    deploy

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,17 +2,20 @@
     <v-app>
         <Nav />
         <v-main>
-            <router-view />
+            <Home />
         </v-main>
     </v-app>
 </template>
 
 <script>
 import Nav from '@/components/layouts/Nav'
+import Home from '@/views/Home'
+
 export default {
     name: 'App',
     components: {
         Nav,
+        Home,
     },
 };
 </script>
--- client/src/main.js
+++ client/src/main.js
@@ -2,12 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
-import axios from 'axios'
 
 Vue.config.productionTip = false
-
-axios.defaults.baseURL = 'http://0.0.0.0:8888/'
-axios.defaults.withCredentials = true
 
 new Vue({
     router,
